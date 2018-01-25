e1cd184b432371833482aa1a0eb994b43b64c7ed
Harsh Girdhar <girdharharsh01@gmail.com>
2018-01-24 23:42:47 -0800

    {feature} - Added Todo Application Phase-I

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, reorderArray, ToastController } from 'ionic-angular';
+
+import { TodoProvider } from "../../providers/todo/todo";
+import { ArchivedTodosPage } from "../archived-todos/archived-todos";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,98 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public todos = [];
+  public reorderIsEnabled = false;
 
+  constructor(private toastController: ToastController, private todoService: TodoProvider, public navCtrl: NavController, private alertController: AlertController) {
+    this.todos = this.todoService.getTodos();
+  }
+
+  toggleReorder() {
+    this.reorderIsEnabled = !this.reorderIsEnabled;
+  }
+
+  itemReordered($event) {
+    reorderArray(this.todos, $event);
+  }
+
+  goToArchivePage() {
+    this.navCtrl.push(ArchivedTodosPage);
+  }
+
+  archiveTodo(todoIndex) {
+    this.todoService.archiveTodo(todoIndex);
+  }
+
+  openTodoAlert() {
+    let addTodoAlert = this.alertController.create({
+      title: "Add A Todo",
+      message: "Enter Your Todo",
+      inputs: [
+        {
+          type: "text",
+          name: "addTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Todo",
+          handler: (inputData) => {
+            let todoText ;
+            todoText = inputData.addTodoInput;
+            this.todoService.addTodo(todoText);
+
+            addTodoAlert.onDidDismiss(() => {
+              let addTodoToast = this.toastController.create({
+                message: "TODO Added",
+                duration: 2000
+              });
+              addTodoToast.present();
+            });
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
+  }
+
+
+  editTodoAlert(todoIndex) {
+    let editTodoAlert = this.alertController.create({
+      title: "Edit A Todo",
+      message: "Edit Your Todo",
+      inputs: [
+        {
+          type: "text",
+          name: "editTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Edit Todo",
+          handler: (inputData) => {
+            let todoText ;
+            todoText = inputData.editTodoInput;
+            this.todoService.editTodo(todoText, todoIndex);
+
+            editTodoAlert.onDidDismiss(() => {
+              let editTodoToast = this.toastController.create({
+                message: "TODO Edited",
+                duration: 2000
+              });
+              editTodoToast.present();
+            });
+          }
+        }
+      ]
+    });
+    editTodoAlert.present();
   }
 
 }
